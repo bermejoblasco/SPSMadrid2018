3f9d3d5a906d3a2946d3ddd6df23cf8db7fced3d
Robert Bermejo <pollobull12@hotmail.com>
2018-05-29 19:27:39 +0200

    first commit

---
+++ FuncionPitufa/Pitufos.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FuncionPitufa
+{
+    public static class Pitufos
+    {
+        [FunctionName("EventGridTriggerPitufos")]
+        public static void Run([EventGridTrigger]JObject eventGridEvent, TraceWriter log)
+        {
+            log.Info(eventGridEvent.ToString(Formatting.Indented));
+        }
+    }
+}
